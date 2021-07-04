19be28926b634b472dbab6a55e93b4d324a16d86
Radviger <radviger@gmail.com>
2021-07-04 17:24:54 +0200

    Updated pendulum example.

--- src/main/java/Pendulum.java
+++ src/main/java/Pendulum.java
@@ -4,13 +4,14 @@ import org.lwjgl.glfw.GLFW;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
+import static lib.math.RenderMaths.format;
 
 public class Pendulum extends Window {
     private final Texture logo = Texture.load("logo.png");
 
     public Pendulum() {
         super(800, 800, "Pendulum", true, "Cambria Math", 46);
-        setIcon("gravity.png");
+        setIcon("pendulum.png");
     }
 
     @Override
@@ -30,7 +31,7 @@ public class Pendulum extends Window {
     double originX = width / 2.0;
     double originY = height / 2.0;
 
-    double angle = Math.PI / 4.0;
+    double angle = Math.PI / 40.0;
     double gravity = 1.0;
     double velocity = 0.0;
     double length = 400.0;
@@ -44,30 +45,36 @@ public class Pendulum extends Window {
             double dx = cursorX - originX;
             double dy = cursorY - originY;
             length = Math.sqrt(dx * dx + dy * dy);
-            angle = Math.atan2(dy, -dx) - Math.PI / 2.0;
+            angle = Math.atan2(-dy, dx) + Math.PI / 2.0;
+        } else {
+            double force = gravity * sin(angle);
+            double acceleration = -force / length;
+
+            velocity += acceleration;
+            angle += velocity;
         }
-
-        double force = gravity * sin(angle);
-        double acceleration = -force / length;
-
-        velocity += acceleration;
-        angle += velocity;
 
         double bobX = length * sin(angle) + originX;
         double bobY = length * cos(angle) + originY;
 
-        velocity *= 0.999;
+        //velocity *= 0.9999;
 
         if (mousePressed) {
-            canvas.drawCircle(0x000000, originX, originY, length, 1.0);
+            canvas.drawCircle(0, originX, originY, length, 1.0);
         }
 
-        canvas.drawLine(0x000000, originX, originY, bobX, bobY, 1.5);
+        canvas.fillCircle(0, originX, originY, 2.0);
+        canvas.drawLine(0, originX, originY, bobX, bobY, 2.0);
+        canvas.fillCircle(0, bobX, bobY, 22.0);
+        canvas.fillCircle(0xFFAA00, bobX, bobY, 20.0);
 
-        canvas.fillCircle(0xc0c0c0, bobX, bobY, 20.0);
+        drawLabel(0, "§6α = " + format((int) (Math.toDegrees(angle) % 360.0)));
+        drawLabel(1, "§6ω = " + format(velocity / elapsed));
+        drawLabel(2, "§6v = " + format(velocity * length / elapsed));
+    }
 
-        canvas.drawText(0, 10, 15, "§6Angle: " + ((int) (Math.toDegrees(angle) % 360.0)), true);
-        canvas.drawText(0, 10, 15 + canvas.font.fontHeight + 5, "§6Velocity: " + velocity, true);
+    private void drawLabel(int index, String text) {
+        canvas.drawText(0, 10, 15 + index * (canvas.font.fontHeight + 5), text, true);
     }
 
     public static void main(String[] args) {
